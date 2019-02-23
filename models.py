db2e08607b0a6315c76de65233acfdde3f87b658
javierbustillo <javier.bustillo@upr.edu>
2019-02-22 22:59:13 -0400

    Added shared_whisky table

--- models.py
+++ models.py
@@ -2,6 +2,12 @@ from datetime import datetime
 
 from config import db
 
+shared_whisky = db.Table('shared_whisky',
+                         db.Column('shared_by', db.Integer, db.ForeignKey('user.uid')),
+                         db.Column('shared_to', db.Integer, db.ForeignKey('user.uid')),
+                         db.Column('wid', db.Integer, db.ForeignKey('whisky.wid')),
+                         db.Column('shared_at', db.DateTime, default=datetime.utcnow))
+
 
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
@@ -24,4 +30,4 @@ class Whisky(db.Model):
     description = db.Column(db.String(400), nullable=False)
     rating = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    created_by = db.Column(db.Integer, db.ForeignKey('User.uid'), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey(User.uid), nullable=False)
