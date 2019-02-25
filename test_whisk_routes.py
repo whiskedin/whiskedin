867c50772d43d59829e854c7cdc4315c93081f24
javierbustillo <javier.bustillo@upr.edu>
2019-02-24 20:30:19 -0400

    fixed tests setup

--- test_whisk_routes.py
+++ test_whisk_routes.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_testing import TestCase
 
 from config import db
@@ -6,11 +7,15 @@ from config import db
 
 class LoginRegisterTest(TestCase):
 
-    DATABASE_URI = "sqlite://"
+    DATABASE_URI = "sqlite:////tmp/test.db"
+    db = None
 
     def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = self.DATABASE_URI
+        self.db = SQLAlchemy(app)
+
         return app
 
     def setUp(self):
@@ -19,3 +24,6 @@ class LoginRegisterTest(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def test_(self):
+        self.assertEqual(None, None)
