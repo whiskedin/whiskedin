4769a275c8bc588edf3c10b1577386dd89e11632
javierbustillo <javier.bustillo@upr.edu>
2019-02-26 18:09:32 -0400

    Added more test cases for endpoints

--- whiskedinRESTAPI/test_whisk_routes.py
+++ whiskedinRESTAPI/test_whisk_routes.py
@@ -48,17 +48,32 @@ class UserTests(unittest.TestCase):
         self.assertEqual(response.status, '201 CREATED')
         self.assertIn('access_token', response.json)
 
+    def test_register_username_exists(self):
+        self.create_test_user()
+        response = self.app.post('/register', data={'username': 'username', 'password': 'password'})
+        self.assertEqual(response.status, '400 BAD REQUEST')
+        self.assertEqual(response.json['msg'], 'username already exists')
+
     def test_login_no_user(self):
         response = self.app.post('/login', data={'username': 'username', 'password': 'password'})
         self.assertEqual(response.status, '400 BAD REQUEST')
         self.assertIn('msg', response.json)
 
     def test_login_user_exists(self):
+        self.create_test_user()
+        response = self.app.post('/login', data={'username': 'username', 'password': 'password'})
+        self.assertEqual(response.status, '200 OK')
+        self.assertIn('access_token', response.json)
+
+    def test_login_user_wrong_password(self):
+        self.create_test_user()
+        response = self.app.post('/login', data={'username': 'username', 'password': 'pass'})
+        self.assertEqual(response.status, '400 BAD REQUEST')
+        self.assertEqual(response.json['msg'], 'Invalid username or password')
+
+    def create_test_user(self):
         password = 'password'
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         user = User(username='username', hashed_password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        response = self.app.post('/login', data={'username': 'username', 'password': 'password'})
-        self.assertEqual(response.status, '200 OK')
-        self.assertIn('access_token', response.json)
