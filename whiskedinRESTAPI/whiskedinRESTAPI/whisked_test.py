14264028b2cee061593d07354cde303373628b47
javierbustillo <javier.bustillo@upr.edu>
2019-03-09 21:28:17 -0400

    Acceptance tests

---
+++ whiskedinRESTAPI/whiskedinRESTAPI/whisked_test.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from whiskedinRESTAPI.app import db, app
+
+
+
+class WhiskedTest(unittest.TestCase):
+    def setUp(self):
+        def setUp(self):
+            app.config['TESTING'] = True
+            app.config['WTF_CSRF_ENABLED'] = False
+            app.config['DEBUG'] = False
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + self.TEST_DB
+            self.app = app.test_client()
+            db.drop_all()
+            db.create_all()
