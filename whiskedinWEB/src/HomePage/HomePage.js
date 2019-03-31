0633326c89802c419f3661cd17c55cac19f68571
javierbustillo <javier.bustillo@upr.edu>
2019-03-30 20:57:57 -0400

    Error handling put

--- whiskedinWEB/src/HomePage/HomePage.js
+++ whiskedinWEB/src/HomePage/HomePage.js
@@ -26,7 +26,7 @@ export default class HomePage extends React.Component {
 
     getWhiskeys = (search) => {
 
-        let get = ''
+        let get = '';
         let config = {'Authorization': 'Bearer '.concat(JSON.parse(localStorage.getItem('user')))};
         if(search){
             get = "/whiskies?s=" + search
@@ -36,44 +36,44 @@ export default class HomePage extends React.Component {
         }
         axios.get("https://whiskedin.herokuapp.com" + get, { headers: config }).then(res => {
 
-            let whiskeys = res.data.whiskies
+            let whiskeys = res.data.whiskies;
             if (whiskeys.length === 0) {
                 console.log("No dude");
             }
             else {
 
-                let i = 0
+                let i = 0;
 
                 whiskeys.map(whiskey => {
                     if(i === 0){
                         whiskey = {...whiskey,
-                            idx: i}
+                            idx: i};
                         this.setState(({deck}) => ({
                             deck: [
                             whiskey
                             ]
-                        }))
+                        }));
                         i++
                     } else {
                         whiskey = {...whiskey,
-                            idx: i}
+                            idx: i};
                         this.setState(({deck}) => ({
                             deck: [
                             ...deck,
                             whiskey
                             ]
-                        }))
+                        }));
                         i++
                     }
-                })
+                });
                 this.empty = false;
 
             }
         })
-    }
+    };
 
     handleNext() {
-        const index = this.state.currIndex + 1
+        const index = this.state.currIndex + 1;
         if(index >= 0 && index < this.state.deck.length){
             this.setState((state) => ({
                 currIndex: index
@@ -82,7 +82,7 @@ export default class HomePage extends React.Component {
     }
 
     handleBack() {
-        const index = this.state.currIndex - 1
+        const index = this.state.currIndex - 1;
         if(index >= 0){
             this.setState((state) => ({
                 currIndex: index
@@ -94,11 +94,11 @@ export default class HomePage extends React.Component {
         this.setState(() => ({
             currIndex: index
         }))
-    }
+    };
 
     handleSubmit = whiskey => {
 
-        console.log(whiskey)
+        console.log(whiskey);
 
         let config = {'Authorization': 'Bearer '.concat(JSON.parse(localStorage.getItem('user')))};
         // eslint-disable-next-line no-useless-concat
@@ -106,34 +106,58 @@ export default class HomePage extends React.Component {
             .then(res => {
                 console.log(res);
                 this.getWhiskeys();
-            })
+                if(document.getElementById("id_error") !== null) {
+                    var error_el = document.getElementById("id_error");
+                    error_el.remove();
+                }
+            }).catch( res => {
+                if(document.getElementById("id_error") === null) {
+                    var error_el = document.createElement("err");
+                    error_el.id = "id_error";
+                    error_el.innerText = "Error occured please try again";
+                    var parent = document.getElementById("id_whisk_div");
+                    parent.appendChild(error_el);
+                }
+        });
 
         this.getWhiskeys()
 
 
-      }
+      };
   
     handleExerciseEdit = whiskey => {
 
-        let editedWhiskey = {...whiskey}
+        let editedWhiskey = {...whiskey};
 
-        delete editedWhiskey.created_at
-        delete editedWhiskey.created_by
-        delete editedWhiskey.image
-        delete editedWhiskey.idx
+        delete editedWhiskey.created_at;
+        delete editedWhiskey.created_by;
+        delete editedWhiskey.image;
+        delete editedWhiskey.idx;
 
-        console.log(editedWhiskey)
+        console.log(editedWhiskey);
 
         let config = {'Authorization': 'Bearer '.concat(JSON.parse(localStorage.getItem('user')))};
         // eslint-disable-next-line no-useless-concat
         axios.put("https://whiskedin.herokuapp.com" + "/whiskies", editedWhiskey, { headers: config })
             .then(res => {
-                console.log(res)
-            })
+                console.log(res);
+                if(document.getElementById("id_error") !== null) {
+                    var error_el = document.getElementById("id_error");
+                    error_el.remove();
+                }
+            }).catch( res => {
+            if(document.getElementById("id_error") === null) {
+                var error_el = document.createElement("err");
+                error_el.id = "id_error";
+                error_el.innerText = "Error occured please try again";
+                var parent = document.getElementById("id_whisk_div");
+                parent.appendChild(error_el);
+            }
+        });
         // this.setState(({deck}) => ({
             
         // }))
-    }
+    };
 
     onSearchInputChange = (event) => {
         if(event.target.value) {
@@ -141,12 +165,12 @@ export default class HomePage extends React.Component {
         } else {
             this.getWhiskeys()
         }
-    }
+    };
 
     render() {
-        console.log(this.state)
-        const whiskeyCard= this.state.deck[this.state.currIndex]
-        const deck= this.state.deck
+        console.log(this.state);
+        const whiskeyCard= this.state.deck[this.state.currIndex];
+        const deck= this.state.deck;
 
         // if(!this.state.loaded){
         //     return (
@@ -210,7 +234,7 @@ export default class HomePage extends React.Component {
                     </Grid>
                     <Grid item sm>
 
-                        <div>
+                        <div id="id_whisk_div">
                             {this.state.loaded ?
                             <table>
                                 <tbody>
